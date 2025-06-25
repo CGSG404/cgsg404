9fd56b2e4432b7ab78cd0bc954c2b6f30c2a717e
CGSG404 <casinogurusg404@gmail.com>
2025-06-25 16:57:30 +0700

    Update HeroSlider.tsx

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -61,18 +61,23 @@ const HeroSlider: React.FC = () => {
         <Swiper
           modules={[Autoplay, Pagination, Navigation]}
           loop={true}
+          centeredSlides={true}
           autoplay={{
             delay: 3000,
             disableOnInteraction: false,
           }}
-          spaceBetween={30}
-          slidesPerView={1}
+          spaceBetween={15}
+          slidesPerView={1.3}
           pagination={{ clickable: true }}
           navigation={true}
           breakpoints={{
             640: {
               slidesPerView: 2,
               spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2.5,
+              spaceBetween: 25,
             },
             1024: {
               slidesPerView: 3,
