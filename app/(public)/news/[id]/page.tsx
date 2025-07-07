393d92ff2a05acc7cbd530775aa2b0fd33e8291d
CGSG404 <casinogurusg404@gmail.com>
2025-07-07 21:08:54 +0700

    Update page.tsx

--- app/(public)/news/[id]/page.tsx
+++ app/(public)/news/[id]/page.tsx
@@ -3,9 +3,13 @@ import NewsDetailPage from "@/components/NewsDetailPage";
 
 export const revalidate = 600;
 
-export const generateMetadata = ({ params }: { params: { id: string } }): Metadata => ({
-  title: `News ${params.id} | CGSG`,
-});
+export function generateMetadata(
+  { params }: { params: { id: string } }
+): Metadata {
+  return {
+    title: `News ${params.id} | CGSG`,
+  };
+}
 
 export default function NewsDetail() {
   return <NewsDetailPage />;
